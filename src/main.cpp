c24de24ab2ecc193e630b4d3f6ffa6a41b103cf6
Donitzo <58847200+Donitzo@users.noreply.github.com>
2024-03-16 14:08:32 +0200

    Updated OpenGL attributes.

--- src/main.cpp
+++ src/main.cpp
@@ -387,10 +387,6 @@ int main(int argc, char* argv[]) {
     // Create SDL window
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
     SDL_Window* window = SDL_CreateWindow("PulseKey",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 240, 168,
